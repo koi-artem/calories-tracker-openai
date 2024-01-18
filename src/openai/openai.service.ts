03422e1b78bb4955dc9cb2ad0d090f85b391002d
artem.lopatiy <arteml@koitechs.com>
2024-01-18 21:20:56 +0200

    openai service

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { OpenAI } from 'openai';
 
 @Injectable()
-export class OpenaiService {
+export class OpenaiService implements OnModuleInit {
+  private readonly openai: OpenAI;
   constructor(private readonly configService: ConfigService) {
-    const key = this.configService.get('OPENAI_API_KEY');
-    console.log(key);
+    this.openai = new OpenAI({
+      apiKey: this.configService.get('OPENAI_API_KEY'),
+    });
+  }
+
+  async prompt(content: string) {
+    return this.openai.chat.completions.create({
+      messages: [{ role: 'user', content }],
+      model: 'gpt-3.5-turbo',
+    });
+  }
+
+  async transcript(audioFile: File) {
+    this.openai.audio.transcriptions.create({
+      file: audioFile,
+      model: 'whisper-1',
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    const result = await this.prompt('Say this is test');
+    console.log(result);
   }
 }
