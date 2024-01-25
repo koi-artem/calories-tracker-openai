258ad4e7e751c2e5147d028d94588ac23a4855e7
artem.lopatiy <arteml@koitechs.com>
2024-01-25 21:31:12 +0200

    openai integrated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Hears, Help, On, Start, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 import { OpenaiService } from './openai/openai.service';
 
+const PROMPT =
+  'Я тебе сейчас напишу предложение в котором перечислю, что я съел. Есть правила ответа:\n' +
+  '1) Тебе нужно вернуть мне список следующего формата без какого-либо дополнительного текста "Продукт 1, вес продукта, каллорийность; Продукт 2, вес продукта, каллорийность" \n' +
+  '2) Количественные показатели конвертируй в вес \n' +
+  '3) Для этого продукта и его веса посчитай примерное количество каллорий, учитывай что продукты уже приготовлены';
+
 @Update()
 @Injectable()
 export class AppService {
@@ -24,6 +30,25 @@ export class AppService {
   @On('sticker')
   async onSticker(ctx: Context) {
     await ctx.reply('👍');
+  }
+
+  @On('voice')
+  async onVoice(ctx: Context) {
+    const fileId = (ctx.message as any).voice;
+    const { href } = await ctx.telegram.getFileLink(fileId);
+    const text = await this.openAiService.transcript(href);
+    await ctx.reply(`Запрос ${text}`);
+
+    const output = await this.openAiService.prompt(`${PROMPT} + \n + ${text} `);
+
+    const {
+      choices: [
+        {
+          message: { content },
+        },
+      ],
+    } = output;
+    await ctx.reply(content);
   }
 
   @Hears('audio')
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { OpenAI, toFile } from 'openai';
+import { OpenAI } from 'openai';
+import axios from 'axios';
 import * as fs from 'fs';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class OpenaiService {
@@ -15,17 +17,32 @@ export class OpenaiService {
   async prompt(content: string) {
     return this.openai.chat.completions.create({
       messages: [{ role: 'user', content }],
-      model: 'gpt-3.5-turbo',
+      temperature: 1,
+      model: 'gpt-4',
     });
   }
 
-  async transcript(audioFile: File) {
-    const audio = fs.createReadStream('audio.mp3');
-    const response = await this.openai.audio.transcriptions.create({
+  async transcript(url: string) {
+    const filename = `${uuid()}.ogg`;
+    const fileResponse = await axios({
+      url,
+      method: 'get',
+      responseType: 'stream',
+    });
+
+    await new Promise((resolve, reject) => {
+      const stream = fileResponse.data.pipe(fs.createWriteStream(filename));
+      stream.on('finish', () => resolve(null));
+      stream.on('error', () => reject('Stream error'));
+    });
+
+    const audio = fs.createReadStream(filename);
+    const { text } = await this.openai.audio.transcriptions.create({
       file: audio,
       model: 'whisper-1',
     });
+    await fs.promises.unlink(filename);
 
-    return response.text;
+    return text;
   }
 }
